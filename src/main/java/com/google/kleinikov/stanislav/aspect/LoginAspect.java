c89a63a3170c2a7c99b095cf94aa768ebff97d54
Lenovo <Lenovo@LAPTOP-3722C9A5>
2018-06-13 18:09:24 +0300

    LoginAspect has been created

---
+++ src/main/java/com/google/kleinikov/stanislav/aspect/LoginAspect.java
@@ -0,0 +1,10 @@
+package com.google.kleinikov.stanislav.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoginAspect {
+
+}
